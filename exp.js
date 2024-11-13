5522e313b8670907df3bbc1355372499e7f6701a
rochanreddy <ayireddyrochanreddy123@gmail.com>
2024-11-13 21:37:41 +0530

    Update exp.js

--- exp.js
+++ exp.js
@@ -512,3 +512,17 @@ window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
 
+
+function flipCard(card) {
+  const allCards = document.querySelectorAll('.unique-card');  // Select all unique cards
+
+  // Check if any other cards are flipped, if yes, flip them back
+  allCards.forEach(otherCard => {
+    if (otherCard !== card && otherCard.classList.contains('flipped')) {
+      otherCard.classList.remove('flipped');
+    }
+  });
+
+  // Toggle the flipped state for the clicked card
+  card.classList.toggle('flipped');
+}
